b1d701788d610843a9eacd09c3cf00ee72b652ab
Denis Gorbunov <denis@nodomain.me>
2016-11-29 13:33:11 +0300

    добавил null: false

--- db/migrate/20161128161335_create_cards.rb
+++ db/migrate/20161128161335_create_cards.rb
@@ -1,8 +1,8 @@
 class CreateCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
-      t.text      :original_text
-      t.text      :translated_text
+      t.text      :original_text, null: false
+      t.text      :translated_text, null: false
       t.datetime  :review_date
       t.timestamps
     end
