9038d9f32d2338d80b97472ea0d702e40bf9e714
Denis Gorbunov <denis@nodomain.me>
2016-11-30 18:00:36 +0300

    review_date not null

--- db/migrate/20161128161335_create_cards.rb
+++ db/migrate/20161128161335_create_cards.rb
@@ -3,7 +3,7 @@ class CreateCards < ActiveRecord::Migration[5.0]
     create_table :cards do |t|
       t.text      :original_text, null: false
       t.text      :translated_text, null: false
-      t.datetime  :review_date
+      t.datetime  :review_date, null: false
       t.timestamps
     end
   end
