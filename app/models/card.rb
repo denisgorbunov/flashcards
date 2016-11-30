dd52852da65469bacd410ab4ce19d13219c1463e
Denis Gorbunov <denis@nodomain.me>
2016-11-30 17:46:45 +0300

    Поправил валидацию

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ApplicationRecord
-  validates :original_text, :translated_text, presence: true
+  validates :original_text, :translated_text, :review_date, presence: true
   validate :check_card
-  before_create :set_review_date
+  before_validation :set_review_date
 
   protected
   def check_card
