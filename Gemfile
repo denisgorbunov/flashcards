977f1a76e9391e8cea355f03a000abfd710b39d2
Denis Gorbunov <denis@nodomain.me>
2016-12-01 20:25:34 +0300

    парсер

--- Gemfile
+++ Gemfile
@@ -32,9 +32,9 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-
 gem 'russian', '~> 0.6.0'
 gem 'simple_form'
+gem 'nokogiri'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "nokogiri"
+require "open-uri"
+url = "http://poligloty.blogspot.ru/2013/08/1000-english-words.html"
+html = open(url)
+doc = Nokogiri::HTML(html)
+words = doc.css("div#post-body-6077576339513753770").text.split("\n") #to_s.gsub('<br>','\n')).text.split('\n').map { |x| x.split(/([^\[]+)(\[[^\]]+\])\s?-\s?(.*)/) }
+
+words.map do |w|
+  w.delete
+end
+
+ words.map do |w|
+   word = w.split(/([^\[]+)(\[[^\]]+\])\s?-\s?(.*)/)
+   word.delete_if { |x| x.empty? }
+   Card.create!(original_text: word[4], translated_text: word[1])
+ end
+
