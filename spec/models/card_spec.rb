d7f468b50135f35934cd8db2e05cc0d9747ac282
Denis <denis@nodomain.me>
2016-12-07 23:46:17 +0300

    подключил shoulda

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -5,8 +5,13 @@ RSpec.describe Card, type: :model do
     @card = create(:card)
   end
 
-  it '#set_review_date?' do
+  it '#set_review_date' do
     expect(@card.review_date.strftime("%d/%m/%Y")).to eq (Time.now + 3.days).strftime("%d/%m/%Y")
   end
-end
 
+  it 'original_text, translated_text presence?' do
+    is_expected.to validate_presence_of(:original_text)
+    is_expected.to validate_presence_of(:translated_text)
+  end
+
+end
