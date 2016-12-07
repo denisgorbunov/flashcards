e3a7b7476b742d51e5583f83a8f4d731e8f8932a
Denis <denis@nodomain.me>
2016-12-07 23:53:27 +0300

    ручная проверка валидации

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe Card, type: :model do
     expect(@card.review_date.strftime("%d/%m/%Y")).to eq (Time.now + 3.days).strftime("%d/%m/%Y")
   end
 
+  it '#validate_card' do
+    card = build(:card, original_text: "house", translated_text: "house")
+    expect(card).to be_invalid
+  end
+
   it 'original_text, translated_text presence?' do
     is_expected.to validate_presence_of(:original_text)
     is_expected.to validate_presence_of(:translated_text)
