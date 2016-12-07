a74305052692af1c6f7d02c076f8c1741dc27aa2
Denis <denis@nodomain.me>
2016-12-07 15:35:44 +0300

    переименовал проверку

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Card, type: :model do
     @card = Card.create({original_text: 'привет', translated_text: 'hello'})
   end
 
-  it "must set review_date to Time.now +3.days (#{(Time.now + 3.days).strftime("%d/%m/%Y")})" do
+  it '#set_review_date?' do
     expect(@card.review_date.strftime("%d/%m/%Y")).to eq (Time.now + 3.days).strftime("%d/%m/%Y")
   end
 end
