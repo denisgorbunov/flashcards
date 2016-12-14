3015e0920877d06cf7e3b04c08eda152126e02d3
Denis Gorbunov <denis@nodomain.me>
2016-12-14 17:41:49 +0300

    seventh task

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :user
-  validates :original_text, :translated_text, :review_date, presence: true
+  validates :original_text, :translated_text, :review_date, :user_id, presence: true
   validate :validate_card
   before_validation :set_review_date, on: :create
 
